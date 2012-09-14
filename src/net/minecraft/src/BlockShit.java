a00fd4baca47d83788de866cb2ac537017f9b0ec
Matej Zilak <teo@teo.sk>
2012-09-14 23:52:20 +0200

    First working stuff
    * item hockey puck
    * item wooden hockey stick
    * a shit block (just to test blocks, shit happens)

---
+++ src/net/minecraft/src/BlockShit.java
@@ -0,0 +1,10 @@
+package net.minecraft.src;
+
+public class BlockShit extends Block
+{
+    protected BlockShit(int par1, int par2)
+    {
+        super(par1, par2, Material.wood);
+        this.setCreativeTab(CreativeTabs.tabBlock);
+    }
+}
---
+++ src/net/minecraft/src/mod_Hockey.java
@@ -0,0 +1,34 @@
+package net.minecraft.src;
+
+public class mod_Hockey extends BaseMod {
+
+	public static final Block shit = new BlockShit(200,0).setBlockName("shit");
+	
+	public static final Item hockeyStick = new Item(2000).setItemName("hockeyStick");
+	public static final Item puck = new Item(2001).setItemName("puck");
+	
+	@Override
+	public String getVersion() {
+		// TODO Auto-generated method stub
+		return "1.3.2";
+	}
+
+	@Override
+	public void load() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public mod_Hockey()
+	{
+	    ModLoader.registerBlock(shit);
+	    ModLoader.addName(shit, "Super-crazy-awesome-cool SHIT");
+	    
+	    hockeyStick.iconIndex = ModLoader.addOverride("/gui/items.png", "/hockeymod/hockeyStick.png");
+	    puck.iconIndex = ModLoader.addOverride("/gui/items.png", "/hockeymod/puck.png");
+	    ModLoader.addName(hockeyStick, "Wooden Hockey Stick");
+     	ModLoader.addName(puck, "Hockey Puck");
+
+	}
+
+}
